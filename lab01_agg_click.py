084d270ee685913e41820440672a59cf902b44e3
Nikolay Potapov <rnjv2015@yandex.ru>
2021-03-24 00:27:00 +0300

    fix export_click for nikolay_potapov_lab01

--- lab01_agg_click.py
+++ lab01_agg_click.py
@@ -4,10 +4,6 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils import timezone
 from datetime import datetime, timedelta
 
-def export_click():
-    print('Hello world!')
-    return 'Hello world!'
-
 args = {
     'owner': 'airflow',
     'start_date': timezone.utcnow() - timedelta(hours=1)
@@ -17,9 +13,18 @@ dag = DAG(
     dag_id='nikolay_potapov_lab01',
     default_args=args,
     description='Simple DAG export agg data to Clickhouse',
-    schedule_interval=None
+    schedule_interval=None,
 )
 
-start = PythonOperator(task_id='export to Clickhouse', python_callable=export_click, dag=dag)
+def export_click(ds, **kwargs):
+    print(ds)
+    return 'Hello world!'
+
+start = PythonOperator(
+    task_id='export to Clickhouse',
+    provide_context=True,
+    python_callable=export_click,
+    dag=dag,
+)
 
 start
