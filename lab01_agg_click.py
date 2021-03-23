11c48b8dba2be7e93bac4617601f3ad9d8be638b
Nikolay Potapov <rnjv2015@yandex.ru>
2021-03-24 01:36:18 +0300

    fix export_click

--- lab01_agg_click.py
+++ lab01_agg_click.py
@@ -3,6 +3,7 @@ from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils import timezone
 from datetime import datetime, timedelta
+from clickhouse_driver import Client
 
 args = {
     'owner': 'airflow',
@@ -17,8 +18,27 @@ dag = DAG(
 )
 
 def export_click():
-    print('Hello from export_click')
-    return 'Hello world!'
+    client = Client(host='localhost', port=9090)
+
+    query = """
+    insert into nikolay_potapov_lab01_agg_hourly
+    with (select max(timestamp) from nikolay_potapov_lab01_rt) as max_dt
+    select
+    min(timestamp)                                                              as ts_start,
+    max(timestamp)                                                              as ts_end,
+    sum(case when eventType = 'itemBuyEvent' then item_price end)               as revenue,
+    count(distinct(case when eventType = 'itemBuyEvent' then partyId end))      as buyers,
+    count(distinct partyId)                                                     as visitors,
+    count(distinct(case when eventType = 'itemBuyEvent' then sessionId end))    as purchases,
+    (revenue / purchases)                                                       as aov,
+    now()                                                                       as tech_agg_ts
+    from nikolay_potapov_lab01_rt 
+    where (detectedDuplicate = 0 and detectedCorruption = 0) and toStartOfHour(timestamp) != toStartOfHour(max_dt)
+    group by toStartOfHour(timestamp)
+    """
+
+    res = client.execute(query)
+    return 'SUCCESS'
 
 start = PythonOperator(
     task_id='export_to_clickhouse',
